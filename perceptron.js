6f47f5a3f57ad338dfcfe53ca754081cb1a5278b
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2024-06-20 12:57:38 -0700

    fix: desired should be 1 or -1, not 1 or 1

--- perceptron.js
+++ perceptron.js
@@ -1,10 +1,12 @@
+
+
 class Perceptron {    
     // Create weights
-    constructor(n) {
-        this.weights = [];
-        this.learningConstant = 0.0001;
+    constructor(n, c) {
+        this.weights = new Array(n + 1); // +1 for bias
+        this.learningConstant = c;
         
-        for (let i = 0; i < n; i++) {
+        for (let i = 0; i < n + 1; i++) {
             this.weights[i] = Math.random() * 2 - 1;
         }
     }
@@ -15,6 +17,8 @@ class Perceptron {
         for (let i = 0; i < inputs.length; i++) {
             sum += inputs[i] * this.weights[i];
         }
+
+        sum += this.weights[this.weights.length - 1];  // Add bias weight
 
         return this.activate(sum);
     }
@@ -27,13 +31,16 @@ class Perceptron {
     train(inputs, desired) {
         let guess = this.feedForward(inputs);
 
+        // This value can only be 0, -2, or 2
         let error = desired - guess;
-        for (let i = 0; i < this.weights.length; i++) {
-            // Train the network against known data.
-            this.weights[i] = this.weights[i] + error * inputs[i] * this.learningConstant;
-        }        
+        
+        // Update weights
+        for (let i = 0; i < this.weights.length - 1; i++) {
+            this.weights[i] += error * inputs[i] * this.learningConstant;
+        }
 
-        console.log(this.weights);
+        // Update bias
+        this.weights[this.weights.length - 1] += error * this.learningConstant;
     }
 }
 
@@ -44,23 +51,24 @@ class Perceptron {
 // y = 1/2 x - 1
 function f(x) {
     return 0.5 * x - 1;
+
 }
-
-
 
 // 3 weight perceptron (2 inputs, 1 bias)
 
-let perceptron = new Perceptron(3);
+let perceptron = new Perceptron(2, 0.0001);
 
 // The input is three values: x, y, and the bias.
-let inputs = [50, -12, 1];
+let inputs = [10, 6];
 console.log(`Inputs: ${inputs}`);
 
 let guess = perceptron.feedForward(inputs);
 console.log(`Guess #1: ${guess}`);
 
+console.log('TRAINING');
+
 // TRAIN
-let trainingLoopCount = 1000;
+let trainingLoopCount = 100000;
 
 for (let i = 0; i < trainingLoopCount; i++){
     let x0 = Math.random() * 200 - 100;
@@ -69,10 +77,13 @@ for (let i = 0; i < trainingLoopCount; i++){
 
     let trainingInputs = [x0, x1, xbias];
     let yVal = f(x0);
-    let desired = (yVal > x1) ? 1 : 0;
+    let desired = (x1 > yVal) ? 1 : -1;
     perceptron.train(trainingInputs, desired);
 }
 
-
-
-// perceptron.train()
+let x = 10;
+console.log(`Actual f(x): ${f(x)}`);
+for (let i = 0; i < 20; i++){
+    guess = perceptron.feedForward([x, i, 1]);
+    console.log(`Inputs: ${x}, ${i}, 1. Guess #${i + 2}: ${guess}`);
+}
